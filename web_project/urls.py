1a803a61423888025fc79ae7018fe636c0a03acf
Gulshan <shrigulshankumar@gmail.com>
2024-05-28 13:16:14 +0300

    Added urls of apps

--- web_project/urls.py
+++ web_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import home
+from home.views import *
+import accounts
+from accounts.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('', include('accounts.urls'))
 ]
